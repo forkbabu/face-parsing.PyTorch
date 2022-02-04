87c415277e9a73f252313e49ec4ee6ec3e4a6e70
forkbabu <sayantandas30011998@gmail.com>
2022-02-03 22:18:31 -0500

    vb

---
+++ log2wandb.py
@@ -0,0 +1,2 @@
+import wandb
+wandb.init(project="faceparsing", entity="ucalyptus",name="train")
--- train.py
+++ train.py
@@ -24,6 +24,7 @@ import datetime
 import argparse
 
 
+
 respth = './res'
 if not osp.exists(respth):
     os.makedirs(respth)
